6fadfc03213a2525707c7a970378cb56a6b3c215
aaashirbad <texoashirbad@gmail.com>
2026-07-05 11:27:46 +0000

    update

---
+++ basic/12_numberAndMath.js
@@ -0,0 +1,21 @@
+const score=100;
+let balance=new Number(score);
+ let z=balance.toString();
+ console.log(balance);
+ console.log(typeof(z));
+ console.log(balance.toFixed(2));
+ console.log(balance.toFixed(1));
+
+ const num=23.823;
+ console.log(num.toPrecision(2));
+ const num1=23.227;
+ console.log(num1.toPrecision(2));
+ console.log(num1.toPrecision(1));
+ console.log(num1.toPrecision(3)); 
+ console.log(num1.toPrecision(4)); 
+ 
+
+
+ let abc=10000000;
+ console.log(abc.toLocaleString());
+ console.log(abc.toLocaleString('en-IN'));
