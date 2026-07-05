b4d6648f5b0dc65b7e8342a601dca688afc480c0
aaashirbad <texoashirbad@gmail.com>
2026-07-05 07:04:18 +0000

    c

---
+++ basic/10_string.js
@@ -0,0 +1,26 @@
+let a="Ashirbad" ;
+let b=new String("mohanty..");
+let c=a+b;
+console.log(c);
+// console.log(a+b);
+console.log(`my name is${a}${b}`)
+console.log(a[0]);
+console.log(a[3]);
+console.log(a.__proto__);
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.charAt(3))
+console.log(a.charAt(1))
+console.log(a.indexOf('A'))
+console.log(c.substring(0,2));
+console.log(c);
+console.log(c.slice(0,4))
+
+let x="   bunty   "
+console.log(x)
+console.log(x.trim());
+console.log(c.includes("Ash"));
+console.log(c.includes("ash"));
+let z=c.replace("Ashirbad","Bunty")
+console.log(z);
+
