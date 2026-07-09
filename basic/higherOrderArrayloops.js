714c0a95728063bb2f444d51d291a5b2152a981d
aaashirbad <texoashirbad@gmail.com>
2026-07-09 16:57:09 +0000

    update

--- basic/higherOrderArrayloops.js
+++ basic/higherOrderArrayloops.js
@@ -1,15 +1,15 @@
-const numbers=[1,2,3,4,5,6,7];
-for(  let number of numbers){
-    console.log(number)
-}
-for(let r in numbers){
-    console.log(numbers[r])
-}
+// const numbers=[1,2,3,4,5,6,7];
+// for(  let number of numbers){
+//     console.log(number)
+// }
+// for(let r in numbers){
+//     console.log(numbers[r])
+// }
 // const arr=["Ashirbad","majnq","hdn"];
-const arr="Ashirbad"
-for(let str of arr){
-    console.log(str);
-}
+// const arr="Ashirbad"
+// for(let str of arr){
+//     console.log(str);
+// }
 
 
 const map=new Map();
@@ -17,26 +17,74 @@ map.set('IN',"INDIA");
 map.set('usa',"united staxte of america");
 map.set('IN',"INDIA");
 map.set('fr',"france");
-// console.log(map)
+// console.log(map);
 
 
-for(let [n,m] of map){
-    console.log(n);
-    console.log(m);
+// for(let something of map){
+//     console.log(something)
+// }
+//[ 'IN', 'INDIA' ][ 'usa', 'united staxte of america' ][ 'fr', 'france' ]
+
+for(let [something,some] of map){
+    console.log(something,some)
 }
 
 
-let my={
-    name:"Ashirbad",
-    reg:35,
-    sec:'a',
-    cgpa:8.9
 
+
+// for(let [n,m] of map){
+//     console.log(n);
+//     console.log(m);
+// }
+
+
+// let my={
+//     name:"Ashirbad",
+//     reg:35,
+//     sec:'a',
+//     cgpa:8.9
+
+// }
+// for(let [vals,c] in my){
+//     console.log(my[vals,c])
+// }
+
+let game={
+    'game1':"cricket",
+    'game2':"football",
+    'game3':"hocky"
 }
-for(let [vals,c] in my){
-    console.log(my[vals,c])
+for (arr in game)
+{
+    console.log(arr);
+    console.log(game[arr])
 }
 
+const programming=[
+    "java",
+    "python",
+  "javaScript",
+"mongo",
+    "database"
+]
+
+for(count in programming){
+    console.log(count);
+}
+for(count in programming){
+    console.log(programming[count]);
+}
+
+let abc=["hg",2,6,5,"gg"]
+abc.forEach (function (val,index,arr){
+    console.log(val,index,arr)
+// abc.forEach (function (val,arr,index){
+//     console.log(val,arr,index)       it doesnot matter how u call always 1st value,2nd index,then array
+})
+// let abc=["hg",2,6,5,"gg"]
+// abc.forEach((val)=>{
+//     console.log(val)
+// })
 
 
 
